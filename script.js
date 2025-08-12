22536e7dd041cb030250ebf023d286d03432150c
Michael Boateng Mensah <mkboatmens17@gmail.com>
2025-08-12 20:01:00 +0000

    first part of task one

---
+++ script.js
@@ -0,0 +1,14 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const form = document.getElementById('registration-form');
+    const feedbackDiv = document.getElementById('form-feedback');
+
+    form.addEventListener('submit', event => {
+        event.preventDefault();
+
+        let isValid = true;
+
+        
+    })
+
+
+})
