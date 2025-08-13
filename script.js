0920e38c24ccd0b311f7891e95beaa342e4adef6
Michael Boateng Mensah <mkboatmens17@gmail.com>
2025-08-13 06:49:40 +0000

    Code to retrieve user input and initilaize validation variables

--- script.js
+++ script.js
@@ -5,7 +5,12 @@ document.addEventListener('DOMContentLoaded', () => {
     form.addEventListener('submit', event => {
         event.preventDefault();
 
+        const usernameInput = document.getElementById('username').value.trim();
+        const emailInput = document.getElementById('email').value.trim();
+        const passwordInput = document.getElementById('password').value.trim();
+
         let isValid = true;
+        let messages = [];
 
         
     })
